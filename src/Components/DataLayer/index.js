3ca3e99016214ae27843f4888668eb01bdda37c3
John Simmons <johndavidsimmons@gmail.com>
2019-04-23 13:55:50 -0400

    datalayer content

--- src/Components/DataLayer/index.js
+++ src/Components/DataLayer/index.js
@@ -1,22 +1,77 @@
 import React from "react";
+import Figure from "react-bootstrap/Figure";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const DigitalData = () => <code>digitalData</code>;
+const DLreqs = () => (
+  <div>
+    <ul>
+      <li>
+        The data layer object should be called <DigitalData />
+      </li>
+      <li>
+        The values in <DigitalData /> should not persist between pages or other
+        state changes
+      </li>
+      <li>
+        <DigitalData /> keys should not contain empty values. Key/value pairs
+        should be set only when they exist.
+      </li>
+      <li>
+        <DigitalData /> should not contain PII
+      </li>
+      <li>
+        The values in <DigitalData /> should be strings. No integers, booleans,
+        arrays, etc.
+      </li>
+    </ul>
+  </div>
+);
 
 const DataLayer = () => {
   return (
     <div>
       <h3>Data Layer</h3>
       <p>
-        The data layer JavaScript object that contains information about the
-        current state of the application. This typically includes page
-        information such as the URL or query parameters and user information
-        such as the current user's username. It exists in order to streamline
-        analytics collection by reducing the need to scrape the page or run
-        redundant functions.
+        The data layer is a static JavaScript object (called <DigitalData />)
+        that contains information about the current state of the application.
+        This can include:
       </p>
+      <ul>
+        <li>Page information such as URL or query parameters</li>
+        <li>
+          User information such as a username or unique ID (provided the user is
+          logged in.
+        </li>
+        <li>
+          Form information such as the loan purpose the user selected on a lead
+          form.
+        </li>
+      </ul>
       <p>
-        <a href="https://www.w3.org/2013/12/ceddl-201312.pdf" target="_blank">
-          More information on data layers can be found here
-        </a>
+        <strong>
+          On it's own, the data layer object does nothing. It exists in order to
+          streamline data collection by reducing the need to scrape the page for
+          information and to serve as a source of truth for any tool that
+          collects front end data.
+        </strong>
       </p>
+      <Row className="push">
+        <Col sm={8}>
+          <Figure>
+            <Figure.Image src="https://i.imgur.com/TJ5mnmi.png" rounded />
+            <Figure.Caption>
+              A basic data layer object on Rocketmortgage.com
+            </Figure.Caption>
+          </Figure>
+        </Col>
+        <Col sm={4}>
+          <DLreqs />
+        </Col>
+      </Row>
+      <hr />
+      <h4>To add the data layer to your website...</h4>
       <p>
         <a
           href="https://git.rockfin.com/marketing-web/bi-data-layer"
